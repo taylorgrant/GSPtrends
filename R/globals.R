fedeed6135ec07b4e3db7a0903321409f7a7c6f0
taylorgrant <taylorgrant38@gmail.com>
2022-03-20 14:09:26 -0700

    adding all global vars

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,6 @@
-utils::globalVariables(c("categories", "id", "hits", "keyword", "ma"))
+utils::globalVariables(c("categories", "id", "hits", "keyword", "ma",
+                         "category", "ma_raw", "raw_share",
+                         "aes", "corr_tbl", "median", "r", "rstar",
+                         "txt_col", "x", "y", "projname", "ma_share", "z_ma_raw",
+                         ".model", "metric", "season_adjust", "seasonal_strength_year",
+                         "trend_strength", "value", "n", "name"))
